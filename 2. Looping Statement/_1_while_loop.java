fa0a390a5b7824c72bd8940fa93f75f905fb0a84
Altrinjazz <118260601+Altrinjazz@users.noreply.github.com>
2022-12-14 20:32:02 +0530

    1. While Loop
    While loop is know as entry check loop because the condition is checked at the beginning of the body of the loop.

---
+++ 2. Looping Statement/_1_while_loop.java
@@ -0,0 +1,14 @@
+public class _1_while_loop {
+    public static void main(String[] args) {
+        int i=1;
+        int sum=0;
+        while (i<=10) {
+            sum=sum++;
+        }
+        System.out.println("output is "+sum);
+        System.out.println();
+        System.out.println("1. While Loop");
+        System.out.println();
+        System.out.println("While loop is know as entry check loop because the condition is checked at the beginning of the body of the loop.");
+    }
+}
