dfa4eb22e03eefdab61941b5b979cfdcd49eb4e0
Altrinjazz <118260601+Altrinjazz@users.noreply.github.com>
2022-12-14 19:36:18 +0530

    1.if statement
    if statement is the simplest of all control statment.it makes one way based on single condition . if the condition is true body of if block executed otherwise not.

--- 1.Control Statements/_1_if_statment.java
+++ 1.Control Statements/_1_if_statment.java
@@ -8,8 +8,12 @@ public class _1_if_statment {
             a=b;
             b=a;
         }
-    
-    System.out.println("if statement is the simplest of all control statment.it makes one way based on single condition . if the condition is true body of if block executed otherwise not.");
-    System.out.println("output is "+a+"  "+b);
+        System.out.println("output is "+a+"  "+b);
+        System.out.println();
+        System.out.println("1. if statment");
+        System.out.println();
+        System.out.println("if statement is the simplest of all control statment.");
+        System.out.println("it makes one way based on single condition .");
+        System.out.println(" if the condition is true body of if block executed otherwise not.");
     }
 }
