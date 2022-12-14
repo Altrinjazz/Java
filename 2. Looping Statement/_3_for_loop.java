c2955cb987eeb7eeb5049b121a8c7e7bbcd11389
Altrinjazz <118260601+Altrinjazz@users.noreply.github.com>
2022-12-15 00:22:28 +0530

    3. For Loop
     for loop is known as entry check loop because the condition is checked at the beginning of the body of the loop.

---
+++ 2. Looping Statement/_3_for_loop.java
@@ -0,0 +1,14 @@
+public class _3_for_loop {
+    public static void main(String[] args) {
+        int i,sum=0;
+        for(i=1;i<=10;i++)
+        {
+            sum=sum+1;
+        }
+        System.out.println("output is "+sum);
+        System.out.println();
+        System.out.println("3. For Loop");
+        System.out.println();
+        System.out.println(" for loop is known as entry check loop because the condition is checked at the begining of the body of the loop.");
+   }
+}
