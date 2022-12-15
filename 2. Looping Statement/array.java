5e7273e4ff0ed8cddf761036b293833f54eb77df
Altrinjazz <118260601+Altrinjazz@users.noreply.github.com>
2022-12-15 15:17:32 +0530

    Create array.java

---
+++ 2. Looping Statement/array.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class array {
+    public static void main(String[] args) {
+        ArrayList <Integer> arr = new ArrayList <Integer> ();
+        arr.add (20);
+        arr.add(30);
+        arr.add(40);
+        System.out.println(arr);
+        
+    }
+}
