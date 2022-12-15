2abb7c821d5b04c376989cc6d334bacd201dd65e
Altrinjazz <118260601+Altrinjazz@users.noreply.github.com>
2022-12-15 21:35:29 +0530

    4. Enhanced for loop
    for loop is enhanced to work on arrays or collections by retrieving each element one by one.

---
+++ 2. Looping Statement/_4_enhanced_for_loop.java
@@ -0,0 +1,15 @@
+public class _4_enhanced_for_loop {
+    public static void main(String[] args) {
+        int a[]={10,20,30,40,50};
+        int sum=0;
+        for(int i:a)
+        {
+            sum=sum+1;
+        }
+        System.out.println("output is "+sum);
+        System.out.println();
+        System.out.println("4. Enhanced for loop");
+        System.out.println();
+        System.out.println("for loop is enhanced to work on arrays or collections by retrieving each element one by one.");
+    }
+}
