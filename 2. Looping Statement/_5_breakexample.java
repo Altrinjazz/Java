935caff72c4ec5c2b763efff890d7bcb0af59b00
Altrinjazz <118260601+Altrinjazz@users.noreply.github.com>
2022-12-15 21:55:40 +0530

    5. Break Statement
    When a break statement is encountered inside a loop, the loop is immediately terminated and the program control resumes at the next statement following the loop.

---
+++ 2. Looping Statement/_5_breakexample.java
@@ -0,0 +1,14 @@
+public class _5_breakexample {
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++){  
+            if(i==5){  
+                break;  
+            }  
+        System.out.println(i);
+        } 
+        System.out.println();
+        System.out.println("5. Break Statement");
+        System.out.println();
+        System.out.println("When a break statement is encountered inside a loop, the loop is immediately terminated and the program control resumes at the next statement following the loop.");   
+    }    
+}
